5b15c7d25c1f8e01b44d4f104d86796b334c0c8e
Christopher Kleman <ckleman@crimson.ua.edu>
2022-02-11 20:08:09 -0600

    Initial Commit

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 11 20:05:45 2022
+
+@author: ckleman
+"""
+
+
+print("Hello World!");
+print("This is the original repo file.");
